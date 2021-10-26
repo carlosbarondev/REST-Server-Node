cb6c2cd3c3d674652fc089a44f247e57f012a86d
carlosbarondev <barondev@gmail.com>
2021-10-26 17:14:09 +0200

    Subida de archivos implementada

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
 const { dbConnection } = require('../database/config');
 
@@ -15,7 +16,7 @@ class Server {
             categorias: '/api/categorias',
             productos: '/api/productos',
             usuarios: '/api/usuarios',
-
+            uploads: '/api/uploads'
         }
 
         // Conectar a la base de datos
@@ -43,6 +44,13 @@ class Server {
         // Directorio Público
         this.app.use(express.static('public'));
 
+        // Fileupload - Carga de archivos
+        this.app.use(fileUpload({
+            useTempFiles: true,
+            tempFileDir: '/tmp/',
+            createParentPath: true
+        }));
+
     }
 
     routes() {
@@ -51,6 +59,7 @@ class Server {
         this.app.use(this.paths.categorias, require('../routes/categorias'));
         this.app.use(this.paths.productos, require('../routes/productos'));
         this.app.use(this.paths.usuarios, require('../routes/usuarios'));
+        this.app.use(this.paths.uploads, require('../routes/uploads'));
     }
 
     listen() {
